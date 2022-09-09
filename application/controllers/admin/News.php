1ac8a6eaf00d305725ac1cdae20133bc36d3b99b
dhakalsandesh0 <dhakalsandesh0@gmail.com>
2022-09-09 18:03:17 +0545

    its just test..

--- application/controllers/admin/News.php
+++ application/controllers/admin/News.php
@@ -21,21 +21,21 @@ class News extends CI_Controller {
         // $this->load->view('templates/footer');
     } 
 
-    public function view($slug = NULL)
-    {
-        $data['news_item'] = $this->news_model->get_news($slug);
+    // public function view($slug = NULL)
+    // {
+    //     $data['news_item'] = $this->news_model->get_news($slug);
 
-        if (empty($data['news_item']))
-        {
-                show_404();
-        }
+    //     if (empty($data['news_item']))
+    //     {
+    //             show_404();
+    //     }
 
-        $data['title'] = $data['news_item']['title'];
+    //     $data['title'] = $data['news_item']['title'];
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('news/view', $data);
-        $this->load->view('templates/footer');
-    }
+    //     $this->load->view('templates/header', $data);
+    //     $this->load->view('news/view', $data);
+    //     $this->load->view('templates/footer');
+    // }
 
     public function create()
     {
